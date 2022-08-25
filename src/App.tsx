5f74ac22da9d23506764ceccdd01fd72d7445c88
Pedro Henrique Vieira de Moura <pedrohvdm@gmail.com>
2022-08-25 09:33:43 -0300

    feat: added basic statistics table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
-import * as React from 'react';
+import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar';
 import Navigator from './Components/Navigator';
 import HomeIcon from '@mui/icons-material/Home';
+import StatisticsTable from './Components/StatisticsComponent/StatisticsTable'
 import { cards } from './data';
+import { statistics } from './statistics'
 
  function App() {
   return (
@@ -14,6 +16,7 @@ import { cards } from './data';
       </div>
       <Navigator />
       <Sidebar cards={cards}/>
+      <StatisticsTable statisticsProp={statistics}></StatisticsTable>
   </div>);
 }
 
---
+++ src/statistics.js
@@ -0,0 +1,7 @@
+export const statistics = {
+    demands: 54,
+    variants: 50,
+    activities: 40, 
+    duration: '120 dias',
+    mean: 30,
+}

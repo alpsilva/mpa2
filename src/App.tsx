e6180df35391775a69b80240e2af45f493dc7fb2
Pedro Moura <pedrohvdm@gmail.com>
2022-08-27 17:04:24 -0300

    refactor: files and folders structure to match routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-
-import React from 'react';
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import Navigator from './Components/Navigator';
-import HomeIcon from '@mui/icons-material/Home';
-import StatisticsTable from './Components/StatisticsComponent/StatisticsTable'
-import { cards } from './data';
-import { statistics } from './statistics'
+import AppRoutes from './routes'
 
  function App() {
   return (
-  <div>
-      <div className='home'>
-        <HomeIcon color="primary" sx={{ fontSize: 50 }} />
-      </div>
-      <Navigator />
-      <Sidebar cards={cards}/>
-      <StatisticsTable statisticsProp={statistics}></StatisticsTable>
-  </div>);
+    <AppRoutes />
+  )
 }
 
 export default App;
--- src/pages/MPA2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MPA2 () {
-    return (
-        <div width='100vw' height='100vh'>
-
-        </div>
-    )
-}

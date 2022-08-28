5325480d9ad03bd65c44fb0f5d4eca90e3d16d1f
Pedro Moura <pedrohvdm@gmail.com>
2022-08-28 15:23:09 -0300

    style: background color of the app pages

--- src/pages/MPA2/index.js
+++ src/pages/MPA2/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
 import './style.css'
 import Sidebar from '../../Components/Sidebar';
 import Navigator from '../../Components/Navigator';
 import DFG from '../../Components/DFG/DFG';
 import HomeIcon from '@mui/icons-material/Home';
-import StatisticsTable from '../.../../Components/StatisticsComponent/StatisticsTable'
+import StatisticsTable from '../../Components/StatisticsComponent/StatisticsTable'
 import { cards, statistics } from '../../data';
 
 export default function MPA2 () {
     return (
-        <div width='100vw' height='100vh'>
+        <div width='100vw' height='100vh' style={{backgroundColor:'#BCCADA',}}>
             <div className='home'>
             <HomeIcon color="primary" sx={{ fontSize: 50 }} />
             </div>
             <Navigator />
             <Sidebar cards={cards}/>
-            <StatisticsTable statisticsProp={statistics}></StatisticsTable>
+            <StatisticsTable statisticsProp={statistics} />
             <DFG/>
         </div>
     )
--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Upload () {
     return (
-        <div width='100vw' height='100vh'>
+        <div width='100vw' height='100vh' style={{backgroundColor: '#BCCADA',}}>
         </div>
     )
 }

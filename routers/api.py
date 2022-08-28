d6a5ddc5c64ae6f507c7f3edddc113248d639409
Gabriel Melo <gvmgs@cin.ufpe.br>
2022-08-28 13:54:20 -0300

    leitura de arquivo csv, task RF1.1.1

--- routers/api.py
+++ routers/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from routers import exemplo_besta
+from routers import read_file
 
 router = APIRouter()
 
 router.include_router(exemplo_besta.router)
+router.include_router(read_file.router)

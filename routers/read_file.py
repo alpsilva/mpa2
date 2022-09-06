0ac7a8d90503dcb4e8efcc9d91fa046d8ef2f758
Gabriel Melo <gvmgs@cin.ufpe.br>
2022-09-06 09:03:54 -0300

    upload route

--- routers/read_file.py
+++ routers/read_file.py
@@ -5,12 +5,12 @@ from internal.read_file import read_logs, read_local_logs, get_logs
 from internal import read_file
 
 router = APIRouter(
-    prefix="/bob/Upload", #rota a ser definida pelo
+    prefix="/bob/api/", #rota a ser definida pelo
     tags=['read file'],
     responses={404: {"Bob: read file": "Not found"}}
 )
 
-@router.put("/csv")
+@router.put("/upload")
 async def upload_csv(file: UploadFile):
     try:
         read_logs(file.file)

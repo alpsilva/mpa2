66b393c1c682ed988242a08fd53046af2d336d2e
André Luiz <alps2@cin.ufpe.br>
2022-09-08 23:29:49 -0300

    Cors policy and reload

--- backend/bob_api.py
+++ backend/bob_api.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from routers.api import router as api_routers
 from internal.read_file import get_logs
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_routers) ## Include Routers
 
 @app.get("/")

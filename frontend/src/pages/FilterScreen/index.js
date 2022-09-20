4ab3d638b6b25ff636557fdb10f4a2fac5ce6ed0
Pedro H. Santos <phfcs@cin.ufpe.br>
2022-09-20 19:39:47 -0300

    Update index.js

--- frontend/src/pages/FilterScreen/index.js
+++ frontend/src/pages/FilterScreen/index.js
@@ -42,8 +42,8 @@ export default function FilterScreen () {
                 </Box>
                 <Box sx={{height: '75%', display: 'flex'}}>
                     {SVGConditional(exibitionState, freqSVG, perfSVG)}
-                    <a href={perfSVG}download><button>Download!</button></a>
-                    <a href={freqSVG}download><button>Download!</button></a>
+                    <a href={perfSVG}download><button>Download Perfomace</button></a>
+                    <a href={freqSVG}download><button>Download Frequência</button></a>
                 </Box>
             </Box>
         </Box>

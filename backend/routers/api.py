bda5add2602c79ae6560feacbc2f413ed1e020b7
André Luiz <alps2@cin.ufpe.br>
2022-09-21 08:27:34 -0300

    demanda table backend

--- backend/routers/api.py
+++ backend/routers/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from routers import read_file
 from routers import filters
+from routers import tables
 
 router = APIRouter()
 
 router.include_router(read_file.router)
 router.include_router(filters.router)
+router.include_router(tables.router)

08ada00edc5e812333f060014fa6b26ba7830b0b
Francisco Nascimento <francisco.lira@quintoandar.com.br>
2022-09-22 17:14:42 -0300

    feat(filter): making request to '/bob/api/filter' route whenever one of the filters are changed

--- frontend/src/pages/FilterScreen/index.js
+++ frontend/src/pages/FilterScreen/index.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import DFG from "../../Components/DFG/DFG";
 // import { cards } from "../../Components/Sidebar/Data";
@@ -11,6 +12,7 @@ import ClientsTable from "../../Components/ClientsComponent";
 import DatesTable from "../../Components/DatesComponent";
 import { Box } from "@mui/material";
 import mock from "../../mock.json";
+import axios from 'axios';
 
 function SVGConditional(exibicao, freqSVG, perfSVG) {
   if (exibicao === "frequencia") {
@@ -18,23 +20,49 @@ function SVGConditional(exibicao, freqSVG, perfSVG) {
   }
   return <DFG DFGProps={perfSVG} />;
 }
-export default function FilterScreen() {
+export default function FilterScreen(props) {
+  const navigate = useNavigate();
   const location = useLocation();
-  console.log("location.state.props", location.state.props)
+
   const [exibicao, setExibicao] = useState(location.state.props.filters.exibicao.slice())
   const [client, setClient] = useState("cliente1")
   const [demand, setDemand] = useState("novoSistema")
   const [startDate, setStartDate] = useState("auto")
   const [endDate, setEndDate] = useState("auto")
-
-  console.log("client", client);
-  console.log("demand", demand);
-  console.log("startDate", startDate);
-  console.log("endDate", endDate);
+  const isFirstRender = useRef(true);
 
   const data = location.state.props;
   const freqSVG = data.freq_svg;
   const perfSVG = data.perf_svg;
+
+  useEffect(() => {
+    const updateFilters = async (data) => {
+      await axios.post('//localhost:8081/bob/api/filter', data)
+      .then((response) => {
+          navigate('/Filter', {state:{props: response.data}});
+
+          // data = response.data; // essas 3 linhas é só algo que quero testar, mas acho que não daria certo
+          // freqSVG = data.freq_svg;
+          // perfSVG = data.perfSVG
+      })
+      .catch((e) => {
+          // toast.error('Upload Error');
+          console.log("Deu ruim, hein!\n\n")
+      })
+    }
+
+    if(!isFirstRender.current) {
+    const data = {
+      client: client,
+      demand: demand,
+      startDate: startDate,
+      endDate: endDate
+    }
+    updateFilters(data)
+    } else {
+      isFirstRender.current = false;
+    }
+  }, [client, demand, startDate, endDate, navigate]);
 
   return (
     <Box

f133d88a570d308e12a85f4d7422a581d4d8a47b
Pedro Moura <48687882+phvm@users.noreply.github.com>
2022-10-18 20:43:19 -0300

    asfauytsdfdustadf

--- frontend/src/pages/DemandsScreen/index.js
+++ frontend/src/pages/DemandsScreen/index.js
@@ -112,7 +112,7 @@ export default function DemandsScreen() {
           {rows.map((row) => (
               <Accordion sx={{ margin: "10px 20px" }}>
                   <AccordionSummary>
-                      <Typography>Caso: {row.case_id} <br/></Typography>
+                      <Typography sx={{margin: '5%'}}>Caso: {row.case_id} </Typography>
                       <Typography>Cliente: {row.cliente}</Typography>
                   </AccordionSummary>
                   <AccordionDetails>

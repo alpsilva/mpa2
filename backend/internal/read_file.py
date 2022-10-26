1df93d0d33f53e846c24ec1aa19d68b93eb92340
André Luiz <alps2@cin.ufpe.br>
2022-10-25 22:02:54 -0300

    fixing negative durations in log

--- backend/internal/read_file.py
+++ backend/internal/read_file.py
@@ -4,7 +4,7 @@ import pm4py
 
 def read_logs(file):
     columns_to_datetime = ['dt_inicio', 'dt_fim']
-    df = pd.read_csv(file, parse_dates=columns_to_datetime) 
+    df = pd.read_csv(file, encoding = "utf-8", parse_dates=columns_to_datetime)
     df = pm4py.format_dataframe(df, case_id="demanda_id", activity_key="tarefa", timestamp_key="dt_inicio")
     df = df.rename(columns={"cliente": "case:cliente"})    
     logs = pm4py.convert_to_event_log(df)
